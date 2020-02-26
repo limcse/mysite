1a1952e8187a59e8f3880d6c55cfbacebfc65069
mysite <lirui_326@163.com>
2020-02-26 14:29:44 +0800

    创建article app, 修改app内的models.py
    执行迁移 migrate  
    修改app内的admin.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'article',
 ]
 
 MIDDLEWARE = [
@@ -102,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
 TIME_ZONE = 'UTC'
 
