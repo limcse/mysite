f7e60b03143633447d158ddb29054be335a6a058
mysite <lirui_326@163.com>
2020-02-26 20:34:18 +0800

    测试版

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'article',
+    #我的app
+    'article.apps.ArticleConfig',
 ]
 
 MIDDLEWARE = [
